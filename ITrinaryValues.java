589a0e31e13a7a29a782f32ba94ea100df69f2f4
kamga-me <43290223+kamga-me@users.noreply.github.com>
2018-09-26 15:05:21 +0200

    Add files via upload

---
+++ ITrinaryValues.java
@@ -0,0 +1,25 @@
+package core;
+
+/**
+* Interface with tiny integer number constants declarations for the values of <code>trinary</code> valuespace (<code>yes</code>, <code>no</code> and <code>maybe</code>).<br>
+*
+* @author Marc E. KAMGA
+* @version 1.0
+*/
+public interface ITrinaryValues {
+
+	/**
+	* Constant for value <code>yes</code> from <code>trinary</code> valuespace.
+	*/
+	public static final byte yes = 1;
+	/**
+	* Constant for value <code>no</code> from <code>trinary</code> valuespace.
+	*/
+	public static final byte no = -1;
+	/**
+	* Constant for value <code>maybe</code> from <code>trinary</code> valuespace.
+	*/
+	public static final byte maybe = 0;
+
+
+}
---
+++ IUsualTinyNumbers.java
@@ -0,0 +1,99 @@
+package core;
+
+import core.math.ISpecialMathNumbers;
+
+/**
+* Interface with integer number constants for the two usual/common tiny integer numbers.<br>
+* 
+* @author Marc E. KAMGA
+* @version 1.0
+*/
+public interface IUsualTinyNumbers extends ISpecialMathNumbers {
+	
+	
+//
+// ********** NOW INHERITED FROM INTERFACE ISpecialMathNumbers
+//
+//	/**
+//	* Constant for integer value <code>0</code>.
+//	*/
+//	public static final byte ZERO = 0;
+
+	/**
+	* Constant for integer value <code>-1</code>.
+	*/
+	public static final byte MINUSONE = -1;
+	
+//
+// ********** NOW INHERITED FROM INTERFACE ISpecialMathNumbers
+//
+//	/**
+//	* Constant for integer value <code>1</code>.
+//	*/
+//	public static final byte ONE = 1;
+	/**
+	* Constant for integer value <code>2</code>.
+	*/
+	public static final byte TWO = 2;
+	/**
+	* Constant for integer value <code>3</code>.
+	*/
+	public static final byte THREE = 3;
+	/**
+	* Constant for integer value <code>4</code>.
+	*/
+	public static final byte FOUR = 4;
+	/**
+	* Constant for integer value <code>5</code>.
+	*/
+	public static final byte FIVE = 5;
+	/**
+	* Constant for integer value <code>6</code>.
+	*/
+	public static final byte SIX = 6;
+	/**
+	* Constant for integer value <code>7</code>.
+	*/
+	public static final byte SEVEN = 7;
+	/**
+	* Constant for integer value <code>8</code>.
+	*/
+	public static final byte EIGHT = 8;
+	/**
+	* Constant for integer value <code>9</code>.
+	*/
+	public static final byte NINE = 9;
+	/**
+	* Constant for integer value <code>10</code>.
+	*/
+	public static final byte TEN = 10;
+	/**
+	* Constant for integer value <code>12</code>.
+	*/
+	public static final byte TWELVE = 12;
+	/**
+	* Constant for integer value <code>16</code>.
+	*/
+	public static final byte SIXTEEN = 16;
+	/**
+	* Constant for integer value <code>18</code>.
+	*/
+	public static final byte EIGHTEEN = 18;
+	/**
+	* Constant for integer value <code>19</code>.
+	*/
+	public static final byte NINETEEN = 19;
+	/**
+	* Constant for integer value <code>20</code>.
+	*/
+	public static final byte TWENTY = 20;
+	/**
+	* Constant for integer value <code>31</code>.
+	*/
+	public static final byte THIRTYONE = 31;
+	/**
+	* Constant for integer value <code>32</code>.
+	*/
+	public static final byte THIRTYTWO = 32;
+	
+}
